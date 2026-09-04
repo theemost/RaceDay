1426c381e6d676c8762e3c62dac723fbb1c88864
themostpainful <wickedbeata@gmail.com>
2026-09-04 12:25:41 +0200

    Created Events table

--- RaceDay.sql
+++ RaceDay.sql
@@ -16,3 +16,15 @@ create table EventTypes(
 	eventTypeId int identity(5000, 1) primary key,
 	eventTypeName varchar(50) not null
 );
+
+create table Events(
+	eventId int identity(2000, 1) primary key,
+	eventName varchar(50) not null,
+	description varchar(100) not null,
+	eventDate varchar(11) not null,
+	location varchar(100) not null,
+	organiserId int,
+	foreign key(organiserId) references Users(userId),
+	eventTypeId int,
+	foreign key(eventTypeId) references EventTypes(eventTypeId)
+);

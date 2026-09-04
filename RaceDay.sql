22caed603d8b90f3e87555db029bfdd3d1d6273a
themostpainful <wickedbeata@gmail.com>
2026-09-04 12:22:56 +0200

    Created table named Users

--- RaceDay.sql
+++ RaceDay.sql
@@ -2,5 +2,13 @@ create database RaceDay;
 
 use RaceDay;
 
-
+create table Users(
+	userId int identity(1000, 1) primary key,
+	firstName varchar(50) not null,
+	lastName varchar(50) not null,
+	emailAddress varchar(100) not null,
+	password varchar(100),
+	username varchar(50),
+	role varchar(50)
+);
 
